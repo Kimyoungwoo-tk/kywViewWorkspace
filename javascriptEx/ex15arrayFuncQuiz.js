a424a5bb4f2a45400483a21bf848f807e0a7e0f1
KiHoonShin <153797167+KiHoonShin@users.noreply.github.com>
2024-01-11 18:41:04 +0900

    1/11 수업
    .

---
+++ javascriptEx/ex15arrayFuncQuiz.js
@@ -0,0 +1,35 @@
+let cat = {
+  kind: '고양이',
+  age: 5
+};
+let dog = {
+  kind: '개',
+  age: 4
+};
+let rabbit = {
+  kind: '토끼',
+  age: 0.5
+};
+let hamster = {
+  kind: '햄스터',
+  age: 1
+};
+
+let pets = [cat, dog, rabbit, hamster, cat];
+
+pets.forEach(obj => console.log(obj))
+
+//1. 동물의 kind 가 개 인것을 찾아라
+pets.forEach(obj => {
+  if (obj.kind == '개') console.log(obj)
+})
+
+//2. 동물의 kind 가 고양이가 아닌것만 빼서 배열로 만들어라
+
+
+//3. 총 동물의 평균 나이를 구해라
+result = pets.reduce((age, obj) => {
+  ()
+})
+
+//4. 동물 나이순으로 내림차순 정렬
